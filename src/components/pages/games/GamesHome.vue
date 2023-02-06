e68ac4a4fb33bcf16a9c67e1477c3f527955d2c2
Chaoli Lou <ex012@qq.com>
2023-02-07 00:02:44 +0800

    Sqoosh images

--- src/components/pages/games/GamesHome.vue
+++ src/components/pages/games/GamesHome.vue
@@ -4,19 +4,19 @@
     <div class="container">
       <div class="games-list">
         <game-item
-          img-src="/img/cqu-horror.png"
+          img-src="/img/cqu-horror.webp"
           title="CQU-Horror"
           desc="Try to survive and find out the truth..."
           linkto="#"
         />
         <game-item
-          img-src="/img/sb.png"
+          img-src="/img/coin-era.webp"
           title="Coin Era"
           desc="Play with your friend"
           linkto="#"
         />
         <game-item
-          img-src="/img/into-soil.png"
+          img-src="/img/into-soil.webp"
           title="Into Soil"
           desc="Place your root into the soil..."
           linkto="game/intosoil"
