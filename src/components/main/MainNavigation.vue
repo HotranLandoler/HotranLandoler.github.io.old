96a80b2bdf43a29b51115382d726d0ed026489a4
ChaoliLou <ex012@qq.com>
2023-02-17 09:50:58 +0800

    Add nav link hover effect

--- src/components/main/MainNavigation.vue
+++ src/components/main/MainNavigation.vue
@@ -1,16 +1,24 @@
 <template>
   <ul class="nav-list">
     <li>
-      <router-link to="/art" @click="onNavClicked()">art</router-link>
+      <router-link class="link" to="/art" @click="onNavClicked()"
+        >art</router-link
+      >
     </li>
     <li>
-      <router-link to="/game" @click="onNavClicked()">game</router-link>
+      <router-link class="link" to="/game" @click="onNavClicked()"
+        >game</router-link
+      >
     </li>
     <li>
-      <router-link to="/tool" @click="onNavClicked()">tool</router-link>
+      <router-link class="link" to="/tool" @click="onNavClicked()"
+        >tool</router-link
+      >
     </li>
     <li>
-      <router-link to="/about" @click="onNavClicked()">about</router-link>
+      <router-link class="link" to="/about" @click="onNavClicked()"
+        >about</router-link
+      >
     </li>
   </ul>
 </template>
@@ -25,8 +33,24 @@ function onNavClicked() {
 </script>
 
 <style scoped>
-a:link,
-a:visited {
+.link {
+  position: relative;
+}
+.link::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  background-color: #777;
+  width: 100%;
+  height: 0.15rem;
+  transform: scaleX(0);
+  transition: transform 0.3s ease-out;
+}
+
+.link:link,
+.link:visited {
+  padding: 0.2rem 0;
   font-size: 1.1rem;
   letter-spacing: 3px;
   text-transform: uppercase;
@@ -36,9 +60,10 @@ a:visited {
   transition: border-bottom 0.2s;
 }
 
-a:hover,
-a:active {
-  border-bottom: 1px solid #333;
+.link:hover::after,
+.link:active::after {
+  /* border-bottom: 1px solid #333; */
+  transform: scaleX(1);
 }
 .nav-list {
   list-style: none;
